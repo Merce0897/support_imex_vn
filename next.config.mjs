6f0f92c66e727bbb4e46c62793fbd1d07ea23aa5
Merce0897 <truongtuanvinh0801@gmail.com>
2024-06-25 18:02:02 +0700

    update config to write file serverless

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    experimental: {
+        outputFileTracingIncludes: {
+            '/api/khai-bao-hoa-chat': ['./tmp/**/*'],
+        },
+    },
 };
 
 export default nextConfig;
